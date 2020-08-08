9b0d2cb1fea69179dcdb42392ce835f61f12b7eb
Rehab-Ahmed253 <68018056+Rehab-Ahmed253@users.noreply.github.com>
2020-08-08 18:38:20 +0400

    part1-submission

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,8 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  );
+};
 
 const App = () => {
   const course = {
